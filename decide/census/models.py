c41ff7284482202176f6e1551e1c75b8cef39492
maria <marcascal2@alum.us.es>
2021-02-06 12:36:52 +0100

    Practica 1 acabada

--- decide/census/models.py
+++ decide/census/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
+    adscription = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
-        unique_together = (('voting_id', 'voter_id'),)
+        unique_together = (('voting_id', 'voter_id', 'adscription'),)
